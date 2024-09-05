27de96dd8b454ac3c89ae8c742412109d837f91d
Jason Tuan Nguyen <tuan.nguyen@bliksund.com>
2024-09-05 16:19:26 +0700

    Init Project

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import '@popperjs/core';
+import _ from 'lodash';
+import * as bootstrap from 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all'
+
+import '../scss/vendor.scss';
+import '../scss/app.scss';
+
+$('h1').css('background', 'purple');
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+'use strict'
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    //mode: 'development',
+    stats: {
+        warnings:false
+    },
+    entry: {
+        app: './src/js/app.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        moduleIds: "hashed",
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+        ],
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                vendor: {
+                    name: "vendor",
+                    test: /[\\/]node_modules[\\/]/
+                }
+            },
+        },
+        runtimeChunk: "single",
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            _: 'lodash',
+        }),
+        new HtmlWebpackPlugin({
+            chunksSortMode: 'manual',
+            hash: true,
+            inject: 'body',
+            minify: true,
+            filename: "index.html",
+            template: "./src/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+                runtimeErrors: true,
+            },
+        },
+        compress: true,
+        port: 9000,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 1032000,
+                    mimetype: 'application/font-woff',
+                },
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                loader: 'file-loader',
+                options: {
+                    esModule: false,
+                },
+            },
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },            
+        ],
+    }
+}
